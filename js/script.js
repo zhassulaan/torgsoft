6064e67f24a359a066b5c40c0beaa6d74047d835
Zhassulan Serikuly <74059375+zhassulaan@users.noreply.github.com>
2022-08-16 01:43:04 +0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+function swipeTo(elem_id) {
+	let elems = document.getElementsByClassName('products_data');
+	let btns = document.getElementsByClassName('swipe_btn');
+	for (let el of elems) {
+		el.classList.remove('_active');
+	}
+	let btn = null;
+	for (let el of btns) {
+		el.classList.remove('_active');
+		if (el.dataset.swipeId == elem_id) {
+			btn = el;
+		}
+	}
+	let el = document.getElementById(`products_data_no_${elem_id}`)
+	el.classList.add('_active');
+	if (btn) {
+		btn.classList.add('_active');
+	}
+}
+
+function load() {
+	swipeTo(1);
+	let swipe_btns = document.getElementsByClassName('swipe_btn');
+	for (let btn of swipe_btns) {
+		console.log(btn.dataset);
+		let swipe_id = btn.dataset.swipeId;
+		btn.onclick = () => {
+			swipeTo(swipe_id);
+		}
+	}
+}
