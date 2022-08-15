729b62c0be8ca363a33377b077d99d216f5f4f8d
Zhassulan Serikuly <74059375+zhassulaan@users.noreply.github.com>
2022-08-16 04:18:13 +0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,11 @@
 function swipeTo(elem_id) {
 	let elems = document.getElementsByClassName('products_data');
 	let btns = document.getElementsByClassName('swipe_btn');
+
 	for (let el of elems) {
 		el.classList.remove('_active');
 	}
+
 	let btn = null;
 	for (let el of btns) {
 		el.classList.remove('_active');
@@ -11,6 +13,7 @@ function swipeTo(elem_id) {
 			btn = el;
 		}
 	}
+
 	let el = document.getElementById(`products_data_no_${elem_id}`)
 	el.classList.add('_active');
 	if (btn) {
@@ -18,9 +21,11 @@ function swipeTo(elem_id) {
 	}
 }
 
-function load() {
-	swipeTo(1);
+function mapSwipeBtns(){
 	let swipe_btns = document.getElementsByClassName('swipe_btn');
+	let prev_btn = document.querySelector('.page_prev.swipe_page');
+	let next_btn = document.querySelector('.page_next.swipe_page');
+
 	for (let btn of swipe_btns) {
 		console.log(btn.dataset);
 		let swipe_id = btn.dataset.swipeId;
@@ -28,4 +33,27 @@ function load() {
 			swipeTo(swipe_id);
 		}
 	}
+	
+	prev_btn.onclick = function () {
+		for (let btn of swipe_btns) {
+			if (btn.classList.contains('_active')) {
+				swipeTo(Number(btn.dataset.swipeId) == 1 ? 3 : Number(btn.dataset.swipeId) - 1);
+				break;
+			}
+		}
+	}
+	
+	next_btn.onclick = function () {
+		for (let btn of swipe_btns) {
+			if (btn.classList.contains('_active')) {
+				swipeTo(Number(btn.dataset.swipeId) == 3 ? 1 : Number(btn.dataset.swipeId) + 1);
+				break;
+			}
+		}
+	}
+}
+
+function load() {
+	swipeTo(1);
+	mapSwipeBtns();
 }
